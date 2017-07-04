ec488e4c7240991c4cba6033af44c26f3f842ef5
Stephan Gabler <stephan.gabler@gmail.com>
2017-07-04 15:52:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tell Stephan what to do, for a whole week.
---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+BigCalendar.setLocalizer(
+  BigCalendar.momentLocalizer(moment)
+);
+
+class App extends Component {
+
+  state = {
+    events: []
+  }
+
+  handleSelectSlot = slotInfo => {
+    this.setState({
+      events: [...this.state.events, {
+        start: slotInfo.start,
+        end: slotInfo.end
+      }]
+    })
+  }
+
+  render() {
+    const {events} = this.state
+    return (
+      <div>
+        <BigCalendar
+          selectable
+          toolbar={false}
+          events={events}
+          defaultView='week'
+          scrollToTime={new Date(1970, 1, 1, 6)}
+          defaultDate={new Date(2015, 3, 12)}
+          onSelectEvent={event => alert(event.title)}
+          onSelectSlot={this.handleSelectSlot}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
