513d76297e705e2de9d73a3961da5a7dce9015cd
Stephan Gabler <stephan.gabler@gmail.com>
2017-07-12 10:27:37 +0200

    Clean up EventDialog and add fields for start and title

--- src/EventDialog.js
+++ src/EventDialog.js
@@ -3,62 +3,65 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
 import moment from 'moment'
 window.moment = moment
 
 export default class EventDialog extends Component {
 
-  state = {
-    localEvent: null,
-  }
+  state = {}
 
   componentWillReceiveProps(nextProps, oldProps) {
     if (nextProps.selectedEventId && nextProps.selectedEventId !== oldProps.selectedEventId) {
-      this.setState({localEvent: {...nextProps.events[nextProps.selectedEventId]}})
+      this.setState({...nextProps.events[nextProps.selectedEventId]})
     }
   }
 
   handleLengthChange = (event, index, value) => {
-    const {localEvent} = this.state
-    localEvent.end = localEvent.start + value * 60 * 1000
-    this.setState({localEvent})
+    const {start} = this.state
+    this.setState({end: start + value * 60 * 1000})
   }
 
   handleSaveClick = () => {
     const {onSaveClick, selectedEventId} = this.props
-    const {localEvent} = this.state
-    onSaveClick && onSaveClick(selectedEventId, localEvent)
+    onSaveClick && onSaveClick(selectedEventId, this.state)
   }
 
   render() {
     const {isOpen, onCloseClick, onDeleteEventClick, selectedEventId} = this.props
-    const {localEvent} = this.state
-    let lengthInMinutes
-    if (localEvent) {
-      lengthInMinutes = (localEvent.end - localEvent.start) / (60 * 1000)
+    const {title, start, end} = this.state
+    if (!(start && end)) {
+      return <div></div>
     }
+    const lengthInMinutes = (end - start) / (60 * 1000)
     const actions = [
       <FlatButton label="Close" primary={false} onTouchTap={onCloseClick} />,
       <FlatButton label="Save" primary={true} onTouchTap={this.handleSaveClick} />,
     ];
     return (
       <Dialog title="Edit event" actions={actions} modal={true} open={isOpen}>
-        {localEvent ? <div>
-          <div>from: {new Date(localEvent.start).toLocaleString()}</div>
-          <div>end: {new Date(localEvent.end).toLocaleString()}</div>
-          <br />
-          <SelectField
-            floatingLabelText="Length"
-            value={lengthInMinutes}
-            onChange={this.handleLengthChange} >
-            {Array.from({length: 12}, (value, key) => (key + 1) * 30).map(duration => {
-              return <MenuItem key={duration} value={duration} primaryText={`${duration / 60} h`} />
-            })}
-          </SelectField>
-          <FlatButton
-              label="Delete"
-              onTouchTap={() => onDeleteEventClick(selectedEventId)} />
-        </div> : <div>empty</div>}
+        <TextField
+            floatingLabelText="Title"
+            value={title}
+            onChange={e => this.setState({title: e.target.value})} />
+        <br />
+        <TextField
+            floatingLabelText="Start"
+            disabled={true}
+            value={moment(start).format('dddd hh:mm a')} />
+        <br />
+        <SelectField
+          floatingLabelText="Duration"
+          value={lengthInMinutes}
+          onChange={this.handleLengthChange} >
+          {Array.from({length: 12}, (value, key) => (key + 1) * 30).map(duration => {
+            return <MenuItem key={duration} value={duration} primaryText={`${duration / 60} h`} />
+          })}
+        </SelectField>
+        <br />
+        <FlatButton
+            label="Delete"
+            onTouchTap={() => onDeleteEventClick(selectedEventId)} />
       </Dialog>
     );
   }
