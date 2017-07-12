4e16ff483d66daa3e393b9256bf7a0e469c3266d
Stephan Gabler <stephan.gabler@gmail.com>
2017-07-12 09:42:36 +0200

    Add dropdown to edit length of event

--- src/EventDialog.js
+++ src/EventDialog.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import moment from 'moment'
+window.moment = moment
 
 export default class EventDialog extends Component {
 
@@ -14,9 +18,19 @@ export default class EventDialog extends Component {
     }
   }
 
+  handleLengthChange = (event, index, value) => {
+    const {localEvent} = this.state
+    localEvent.end = localEvent.start + value * 60 * 1000
+    this.setState({localEvent})
+  }
+
   render() {
     const {isOpen, onCloseClick, onDeleteEventClick, onSaveClick, selectedEventId} = this.props
     const {localEvent} = this.state
+    let lengthInMinutes
+    if (localEvent) {
+      lengthInMinutes = (localEvent.end - localEvent.start) / (60 * 1000)
+    }
     const actions = [
       <FlatButton label="Close" primary={false} onTouchTap={onCloseClick} />,
       <FlatButton label="Save" primary={true} onTouchTap={() => onSaveClick} />,
@@ -24,8 +38,17 @@ export default class EventDialog extends Component {
     return (
       <Dialog title="Edit event" actions={actions} modal={true} open={isOpen}>
         {localEvent ? <div>
-          <div>from: {localEvent.start}</div>
-          <div>to: {localEvent.end}</div>
+          <div>from: {new Date(localEvent.start).toLocaleString()}</div>
+          <div>end: {new Date(localEvent.end).toLocaleString()}</div>
+          <br />
+          <SelectField
+            floatingLabelText="Length"
+            value={lengthInMinutes}
+            onChange={this.handleLengthChange} >
+            {Array.from({length: 12}, (value, key) => (key + 1) * 30).map(duration => {
+              return <MenuItem value={duration} primaryText={`${duration / 60} h`} />
+            })}
+          </SelectField>
           <FlatButton
               label="Delete"
               onTouchTap={() => onDeleteEventClick(selectedEventId)} />
@@ -34,3 +57,4 @@ export default class EventDialog extends Component {
     );
   }
 }
+
