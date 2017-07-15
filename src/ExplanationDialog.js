453f58cc9c06ab0f7ce486745b9fe08714a524b7
Stephan Gabler <stephan.gabler@gmail.com>
2017-07-15 13:11:31 +0200

    Add explanation dialog draft

--- src/ExplanationDialog.js
+++ src/ExplanationDialog.js
@@ -10,8 +10,29 @@ export default class ExplanationDialog extends Component {
       <FlatButton label="Close" primary={true} onTouchTap={onCloseClick} />,
     ];
     return (
-      <Dialog title="Explanation" actions={actions} modal={true} open={isOpen}>
-        An Explanation what this experiment is all about.
+      <Dialog
+          title="Explanation" actions={actions} modal={true} open={isOpen}
+          autoScrollBodyContent={true}>
+        <h3>This is an experiment to create the most exciting week anyone has ever had.</h3>
+        <ul>
+          <li>Please pick activities that I usually wouldn't do.</li>
+          <li>Only 14 hours of a day can be filled up with activities.</li>
+          <li>
+            And 8 hours a day are reserved for sleeping, however you can tell me where to sleep!
+            <span style={{color: '#ccc'}}> - tell me to pitch my tent in Humboldthain Park and I'll sleep there.</span>
+          </li>
+          <li>The maximal length of an event is limited to 8 hours.</li>
+          <li>Each event links to the creators FB profile, in case you want to communicate with them.</li>
+          <li>
+            You can edit the events created by other people.
+            <span style={{color: '#ccc'}}> This kind of collaboration is desired, maybe the creator was not aware of how long it would take me to reach the destination or the event has to be changed because of the weather. </span>
+          </li>
+          <li>
+            Please consider preparation times for an event.
+            <span style={{color: '#ccc'}}> If a concert starts at 8, you can make the event start at 7 if I still have to buy the tickets and it is quite far from the previous event.</span>
+          </li>
+          <li>It is ok if some of the events cost some money, but please consider that I'm currently unemployed and on a tight budget. I won't mind getting invited for anything though ;-)</li>
+        </ul>
       </Dialog>
     );
   }
